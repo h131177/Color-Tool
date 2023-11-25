dec456df01bcd7db4283c2afdd73db63dcfe786e
h131177 <54102543+h131177@users.noreply.github.com>
2023-11-26 00:46:40 +0100

    Convert hex color to rgb

--- color.js
+++ color.js
@@ -1,9 +1,4 @@
 
-//Get a reference to hexInput and inputColor DOM elements
-//Create a keyup event handler for hexInput
-//Check if hex color is valid
-//If hex color is valid, update the background color of inputColor
-
 const hexInput = document.getElementById("hexInput");
 const inputColor = document.getElementById("inputColor");
 
@@ -23,4 +18,28 @@ const isValidHex = (hex) => {
     return strippedHex.length === 3 || strippedHex.length === 6;
 }
 
+//Create a function to convert Hex to RGB
+//this should work with 3 or 6 character hex values
+//Hint - use parseInt("", 16) to convert a hex value to a decimal value
+//should return an object with 3 properties - r,g, and b
+//Test your function with a few different use cases
 
+const convertHexToRGB = (hex) => {
+    if(!isValidHex(hex)) return null;
+
+    let strippedHex = hex.replace('#', '');
+
+    if(strippedHex.length === 3) {
+        strippedHex = strippedHex[0] + strippedHex[0]
+        + strippedHex[1] + strippedHex[1]
+        + strippedHex[2] + strippedHex[2];
+    }
+    
+    const r = parseInt(strippedHex.substring(0,2), 16);
+    const g = parseInt(strippedHex.substring(2,4), 16);
+    const b = parseInt(strippedHex.substring(4,6), 16);
+
+    return {r, g, b}
+}
+
+console.log(convertHexToRGB("000"));
